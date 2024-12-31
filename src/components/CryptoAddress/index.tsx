35b4d120ea1d51b5757fc4d02a0bd4fb380ef387
cyrptstacher <papel.enmascarado@gmail.com>
2024-12-31 15:00:36 -0600

    Update index.tsx

--- src/components/CryptoAddress/index.tsx
+++ src/components/CryptoAddress/index.tsx
@@ -5,7 +5,7 @@ import { Copy, Check } from 'lucide-react';
 
 const CryptoAddress = () => {
   const [copied, setCopied] = useState(false);
-  const address = "Coming Soon"; // Replace with your actual crypto address
+  const address = "FxwjyewsutYQpLKRZJuEyG5jJeNwcn4bnsNStDRRpump"; // Replace with your actual crypto address
 
   const copyToClipboard = async () => {
     try {
